d6ee818987889a01c01cd3627a8478e6d3722ba2
Darwin Wang <darwinw@microsoft.com>
2015-09-07 18:09:48 +0800

    Update all package to latest

--- .emacs.d/wttr-config/modes/codesearch.el
+++ .emacs.d/wttr-config/modes/codesearch.el
@@ -1,3 +1,4 @@
 ;; -*- coding: utf-8 -*-
+(wttr/plugin:prepend-to-load-path "dash")
 (wttr/plugin:prepend-to-load-path "codesearch")
 (require 'codesearch)
--- .emacs.d/wttr-config/modes/magit.el
+++ .emacs.d/wttr-config/modes/magit.el
@@ -1,6 +1,6 @@
 ;; -*- coding: utf-8 -*-
 
-(wttr/plugin:prepend-to-load-path "magit-suite/magit")
+(wttr/plugin:prepend-to-load-path "magit-suite/magit/lisp")
 (wttr/plugin:prepend-to-load-path "magit-suite/git-modes")
 (autoload 'magit-status "magit" "magit" t)
 
