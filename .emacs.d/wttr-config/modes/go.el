e38e52635ea0b3d11cacfa08581b0fb9b708f73b
winterTTr <winterTTr@gmail.com>
2015-09-09 13:54:28 +0800

    change go mode path

--- .emacs.d/wttr-config/modes/go.el
+++ .emacs.d/wttr-config/modes/go.el
@@ -2,6 +2,6 @@
 
 (when wttr/os:windowsp
   (wttr/prepend-to-exec-path "C:/go/bin"))
-(wttr/plugin:prepend-to-load-path "go-mode")
+(wttr/plugin:prepend-to-load-path "go-mode.el")
 (require 'go-mode-load)
 
