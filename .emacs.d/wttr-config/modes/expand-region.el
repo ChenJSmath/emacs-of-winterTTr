2188e199744fab0376372172460dada3157c6bb9
winterTTr <winterTTr@gmail.com>
2015-09-09 13:50:16 +0800

    change expand region config

--- .emacs.d/wttr-config/modes/expand-region.el
+++ .emacs.d/wttr-config/modes/expand-region.el
@@ -1,5 +1,5 @@
 ;; -*- coding: utf-8 -*-
 
-(wttr/plugin:prepend-to-load-path "expand-region")
+(wttr/plugin:prepend-to-load-path "expand-region.el")
 (autoload 'er/expand-region "expand-region" "auto expand region" t)
 (global-set-key (kbd "M-2") 'er/expand-region)
