e48851a36a4f1b181e8470da9bd619745593555b
winterTTr <winterTTr@gmail.com>
2015-09-12 19:34:00 +0800

    Update js2 config

--- .emacs.d/wttr-config/modes/js2.el
+++ .emacs.d/wttr-config/modes/js2.el
@@ -1,4 +1,5 @@
 ;; -*- coding: utf-8 -*-
-(wttr/plugin:prepend-to-load-path "js2-mode-20090723")
-(autoload 'js2-mode "js2" nil t)
+(wttr/plugin:prepend-to-load-path "js2-mode")
+(autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
